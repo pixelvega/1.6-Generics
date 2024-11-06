e7d971cc03a005ed3139dae71396ef6b3cfcd3ac
pixelvega <pixelvega.info@gmail.com>
2024-11-06 10:37:37 +0100

    Improvements

--- src/main/java/level2/exercise1/GenericMethods.java
+++ src/main/java/level2/exercise1/GenericMethods.java
@@ -2,7 +2,7 @@ package level2.exercise1;
 
 public class GenericMethods {
 
-    public <U, V> void printArgs(Persona persona, U arg2, V arg3) {
+    public <T> void printArgs(Persona persona, T arg2, T arg3) {
         System.out.println("Arg Persona: " + persona);
         System.out.println("Arg 2: " + arg2);
         System.out.println("Arg 3: " + arg3);
--- src/main/java/level2/exercise2/GenericMethods.java
+++ src/main/java/level2/exercise2/GenericMethods.java
@@ -1,12 +1,10 @@
 package level2.exercise2;
 
-import java.util.Arrays;
-
 public class GenericMethods {
 
     @SafeVarargs
-    public final <T> void printArgs(Persona persona, T... args) {
-        System.out.println("Arg Persona: " + persona);
+    public final <T> void printArgs(T... args) {
+
         for (int i=0; i<args.length; i++) {
             System.out.println("Arg" + i + ": " + args[i] );
         }
